4d7a5cfc60cf0c19869580973af99d3c44f23244
ivge <swordhorde@gmail.com>
2017-07-06 00:54:58 +0300

    Data model refactored

--- rssSandbox/Models/DataModel.cs
+++ rssSandbox/Models/DataModel.cs
@@ -8,19 +8,24 @@ namespace rssSandbox.Models
 {
     public static class DataModel
     {
-        public static HashSet<Feed> Feeds = new HashSet<Feed>()
-        {
-            new RSSFeed("Meduza", new Uri(@"https://meduza.io/rss/all")),
-            new RSSFeed("ЛІГА.Новости", new Uri(@"http://news.liga.net/all/rss.xml")),
-            new RSSFeed("ЛІГА.Бизнес", new Uri(@"http://biz.liga.net/all/rss.xml"))
-        };
+        public static HashSet<Feed> Feeds;
 
-        public static  List<User> Users = new List<User>()
-        {
-            new User("John Doe", "se7en"),
-            new User("Marry Sue", "qwerty"),
-        };
+        public static List<User> Users;
 
+        static DataModel()
+        {
+            Feeds = new HashSet<Feed>();
+            Users = new List<User>();
+        }
+
+        static void Seed()
+        {
+            Feeds.Add(new RSSFeed("Meduza", new Uri(@"https://meduza.io/rss/all")));
+            Feeds.Add(new RSSFeed("ЛІГА.Новости", new Uri(@"http://news.liga.net/all/rss.xml")));
+            Feeds.Add(new RSSFeed("ЛІГА.Бизнес", new Uri(@"http://biz.liga.net/all/rss.xml")));
+            Users.Add(new User("John Doe", "se7en"));
+            Users.Add(new User("Marry Sue", "qwerty"));
+        }
 
 
     }
